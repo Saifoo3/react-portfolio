13d203ce2e7584bed245f9c2099ea3c8d10b1bc0
Saifoo3 <saif.azeem333@gmail.com>
2024-09-28 22:31:12 +0400

    Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,57 @@
+// // import logo from "../assets/saiflogo.png";
+// // import { FaLinkedin } from "react-icons/fa";
+// // import { FaGithub } from "react-icons/fa";
+// // import { FaInstagram } from "react-icons/fa";
+
+// // const Navbar = () => {
+// //   return (
+// //     <nav className="mg-20 flex items-center justify-between py-6">
+// //         <div className="flex flex-shrink-0 items-center">
+// //             <img className="mx-2 w-20" src={logo} alt="logo" />
+// //         </div>
+// //         <div className= "m-6 flex items-center justify-center gap-5 text-2xl ">
+// //         <FaLinkedin />
+// //         <FaGithub />
+// //         <FaInstagram />
+// //         </div>
+// //     </nav>
+// //   )
+// // }
+
+// // export default Navbar;  
+
 // import logo from "../assets/saiflogo.png";
-// import { FaLinkedin } from "react-icons/fa";
-// import { FaGithub } from "react-icons/fa";
-// import { FaInstagram } from "react-icons/fa";
+// import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 // const Navbar = () => {
 //   return (
 //     <nav className="mg-20 flex items-center justify-between py-6">
-//         <div className="flex flex-shrink-0 items-center">
-//             <img className="mx-2 w-20" src={logo} alt="logo" />
-//         </div>
-//         <div className= "m-6 flex items-center justify-center gap-5 text-2xl ">
-//         <FaLinkedin />
-//         <FaGithub />
-//         <FaInstagram />
-//         </div>
+//       <div className="flex flex-shrink-0 items-center">
+//         <a href="/" rel="noopener noreferrer">
+//           <img className="mx-2 w-20" src={logo} alt="logo" />
+//         </a>
+//       </div>
+//       <div className="m-6 flex items-center justify-center gap-5 text-2xl">
+//         <a href="https://www.linkedin.com/in/saifmohammed-azeem-as234/" target="_blank" rel="noopener noreferrer">
+//           <FaLinkedin />
+//         </a>
+//         <a href="https://github.com/Saifoo3?tab=repositories" target="_blank" rel="noopener noreferrer">
+//           <FaGithub />
+//         </a>
+//         <a href="https://www.instagram.com/your-username" target="_blank" rel="noopener noreferrer">
+//           <FaInstagram />
+//         </a>
+//       </div>
 //     </nav>
-//   )
+//   );
 // }
 
-// export default Navbar;  
+// export default Navbar;
+
 
 import logo from "../assets/saiflogo.png";
-import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa"; 
+import { TbFileCv } from "react-icons/tb";
 
 const Navbar = () => {
   return (
@@ -40,6 +70,10 @@ const Navbar = () => {
         </a>
         <a href="https://www.instagram.com/your-username" target="_blank" rel="noopener noreferrer">
           <FaInstagram />
+        </a>
+        {/* Add the Resume icon */}
+        <a href="https://drive.google.com/file/d/1fcSlrDhJl6BYlhddauZPRTZV8qx8jDDu/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <TbFileCv title="View/Download Resume" />
         </a>
       </div>
     </nav>
