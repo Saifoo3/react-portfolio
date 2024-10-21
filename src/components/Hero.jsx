e2df1b9e9e2fd5ca6159706a9656d7cba6302f90
Saifoo3 <saif.azeem333@gmail.com>
2024-10-21 09:52:06 +0400

    #

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/saifheropg.jpg";
+import profilePic from "../assets/saifherowpg.jpg";
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
