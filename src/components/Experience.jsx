f94a4dfd35ffe5a8055a6b10e8fe097ea08d50d3
Saifoo3 <saif.azeem333@gmail.com>
2025-05-19 14:35:35 +0400

    #

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -34,7 +34,7 @@ const Experience = () => {
                 {experience.role} - <span className="text-sm text-purple-100">{experience.company}
                 </span>
               </h6>
-              <p className="mb-4 text-neutral-400 ">{experience.description}</p>
+              <p className="flex flex-wrap gap-x-2 gap-y-2 mb-4 text-neutral-400 ">{experience.description}</p>
               {experience.technologies.map((tech, index) => (
                 <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
               ))}
